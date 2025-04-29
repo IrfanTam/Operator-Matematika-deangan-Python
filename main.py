f4b34d4bbdf5dc8753a36319c4014086ab0a9585
Irfan <timtam2405@gmail.com>
2025-04-29 22:26:17 +0700

    Mengimport fungsi dan membuat printout

--- main.py
+++ main.py
@@ -1,2 +1,12 @@
+from fungsi import jumlah, kurang, kali
+
 a = 2
 b = 3
+
+out1 = jumlah(a,b)
+out2 = kurang(a,b)
+out3 = kali(a,b)
+
+print(f'Hasil penjumlah dari {a} + {b} = {out1}')
+print(f'Hasil penurangan dari {a} - {b} = {out2}')
+print(f'Hasil perkalian dari {a} x {b} = {out3}')
