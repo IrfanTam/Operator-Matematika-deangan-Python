1c343fa0822b024cdea57b4d86d688b1d0d01a4b
Irfan <timtam2405@gmail.com>
2025-04-29 23:09:49 +0700

    Implementasi operator bagi

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fungsi import jumlah, kurang, kali
+from fungsi import jumlah, kurang, kali, bagi
 
 a = 2
 b = 3
@@ -6,7 +6,9 @@ b = 3
 out1 = jumlah(a,b)
 out2 = kurang(a,b)
 out3 = kali(a,b)
+out4 = bagi(a,b)
 
 print(f'Hasil penjumlah dari {a} + {b} = {out1}')
 print(f'Hasil penurangan dari {a} - {b} = {out2}')
 print(f'Hasil perkalian dari {a} x {b} = {out3}')
+print(f'Hasil perkalian dari {a} : {b} = {out4}')
